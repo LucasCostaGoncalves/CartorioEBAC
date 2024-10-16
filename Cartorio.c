5304bffb9bc21e29a7f562ed02e8a1bbb9fde0c9
LucasCostaGoncalves <lc915601@gmail.com>
2024-10-15 21:10:37 -0300

    Adição de uma senha ao programa
    Ao abrir o programa o usuário terá que digitar a senha "admin" para acessar o menu do programa

--- Cartorio.c
+++ Cartorio.c
@@ -69,7 +69,7 @@ int consulta()
 	char conteudo[200];
 	//fim da criaçao de variaveis/string
 	
-	printf("Digite o CPF que deseja consultar: "); //coletando info do usuario
+	printf("Digite o CPF que deseja consultar: "); //recebendo qual usuario vai ser consultado
 	scanf("%s",cpf); //aguardando resposta do usuario
 	
 	FILE *file; //criando um arquivo
@@ -120,53 +120,72 @@ int deletar()
 
 
 int main()
-	{
+{
+	setlocale(LC_ALL, "Portuguese");
+	
 	int opcao=0; //Definindo variáveis
 	int laco=1;
+	char senhadigitada[10]="a";//criando uma senha de login para o sistema
+	int comparacao;
 	
-	for(laco=1;laco=1;)
+	printf("### Cartório da EBAC ###\n\n");
+	printf("Login de administrador!\n\nDigite a sua senha: ");
+	scanf("%s",senhadigitada);
+	
+	comparacao = strcmp(senhadigitada, "admin");
+	
+	if(comparacao == 0)
 	{
-		
-		system("cls"); //responsável por limpar a tela apagando tudo q foi escrito
-	
-	    setlocale(LC_ALL, "Portuguese"); //Definindo a linguagem
-	
-		printf("### Cartório da EBAC ###\n\n"); //Inicio do menu
-		printf("Escolha a opção desejada do menu:\n\n");
-		printf("\t1 - Registrar nomes\n");
-		printf("\t2 - Consultar nomes\n");
-		printf("\t3 - Deletar nomes\n\n");
-		printf("\t4 - Sair do sistema\n\n");
-		printf("opção:"); //Fim do menu
-	
-		scanf("%d",&opcao); //Armazenando a escolha do usuario
-	
-		system("cls"); //responsável por limpar a tela apagando tudo q foi escrito
-		
-		
-		switch(opcao) //inicio da seleção do menu
+		system("cls");
+		for(laco=1;laco=1;)
 		{
-			case 1:
-			registro(); //chamada de funções 
-			break;
 			
-			case 2:
-			consulta();
-			break;
+			system("cls"); //responsável por limpar a tela apagando tudo q foi escrito
+		
+	    	setlocale(LC_ALL, "Portuguese"); //Definindo a linguagem
+		
+			printf("### Cartório da EBAC ###\n\n"); //Inicio do menu
+			printf("Escolha a opção desejada do menu:\n\n");
+			printf("\t1 - Registrar nomes\n");
+			printf("\t2 - Consultar nomes\n");
+			printf("\t3 - Deletar nomes\n\n");
+			printf("\t4 - Sair do sistema\n\n");
+			printf("opção:"); //Fim do menu
+	
+			scanf("%d",&opcao); //Armazenando a escolha do usuario
+	
+			system("cls"); //responsável por limpar a tela apagando tudo q foi escrito
+		
+		
+			switch(opcao) //inicio da seleção do menu
+			{
+				case 1:
+				registro(); //chamada de funções 
+				break;
 			
-			case 3:
-			deletar();
-			break;
+				case 2:
+				consulta();
+				break;
 			
-			case 4:
-			printf("Obrigado por utilizar o sistema!\n\n");
-			return 0;
-			break;
+				case 3:
+				deletar();
+				break;
 			
-			default:
-			printf("Esta opção não existe!\n");
-			system("pause");
-			break;
+				case 4:
+				printf("Obrigado por utilizar o sistema!\n\n");
+				return 0;
+				break;
+			
+				default:
+				printf("Esta opção não existe!\n");
+				system("pause");
+				break;
+			}
 		}
 	}
+	
+	else
+		printf("\nSenha incorreta!");
 }
+
+//system("cls"); //responsável por limpar a tela apagando tudo q foi escrito
